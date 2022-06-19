bf8f1b233fed1bfb14158f667c2669fddb442f2c
RedaMeziouni <meziounidb@gmail.com>
2022-06-19 17:40:57 +0100

    GET product by id

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,8 @@
 const Product = require('../models/productModel')
 
+//@dec Get All the products
+//@route GET api/products
+
 async function getProducts(req, res) {
     try {
         const products = await Product.findAll()
@@ -12,6 +15,29 @@ async function getProducts(req, res) {
     }
 }
 
+//@dec Get Signle product
+//@route GET api/product/:id
+
+async function getProduct(req, res, id) {
+    try {
+        const product = await Product.findById(id)
+
+        if(!product) {
+            res.writeHead(404, { 'Content-Type': 'application/json' })
+            res.end(JSON.stringify({ message: 'Product not Found' }))
+        } else {
+            res.writeHead(200, { 'Content-Type': 'application/json' })
+            res.end(JSON.stringify(product))
+        }
+
+        
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 module.exports = {
-    getProducts
+    getProducts,
+    getProduct
 }
